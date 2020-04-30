d5a74344ba39a28b687c9100de782fe2d9ee86a7
dustin <djarcene@gmail.com>
2020-04-30 22:27:25 +0000

    Automatically backed up by Learn

--- lib/customer.rb
+++ lib/customer.rb
@@ -22,13 +22,5 @@ def meals
   Meal.all.select do |meal|
     meal.customer == self
   end
-  
-  def best_tipper
-  best_tipped_meal = meals.max do |meal_a, meal_b|
-    meal_a.tip <=> meal_b.tip
-  end
- 
-  best_tipped_meal.customer
-end
 end
 
